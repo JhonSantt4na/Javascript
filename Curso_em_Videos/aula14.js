bd26bb83b3a67276b253ceca2e49d497975d3ebe
JSantt4na <jhonnsantt4na@gmail.com>
2024-07-03 22:16:16 -0300

    Aula14

---
+++ Curso_em_Videos/aula14.js
@@ -0,0 +1,14 @@
+
+/* Usando o for 
+for (var c = 1; c <= 10; c++) {
+    console.log(`OI ${c}`);
+}*/
+
+// Passo a Passo
+console.log("Vai comerçar !")
+for (var x = 1; x <= 5; x++) {
+    console.log(`OI ${x}`);
+}
+console.log(" Chegou ao Fim ");
+
+

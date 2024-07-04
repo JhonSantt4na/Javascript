ee1f44e3ccec78430a10bb7b6d4100c3cc0c9129
JSantt4na <jhonnsantt4na@gmail.com>
2024-07-03 22:15:38 -0300

    Aula 13

---
+++ Curso_em_Videos/aula13.js
@@ -0,0 +1,25 @@
+
+// Fazendo manualmente
+
+/*console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")*/
+
+//fazendo com while
+var c = 1;
+while (c <= 4) {
+    console.log(`passo ${c}, Com While ?`)
+    c++
+}
+
+
+//fazendo com do while
+var x = 1;
+do {
+    console.log(`passo ${x}, Com do While?`)
+    x++
+} while (x <= 5)
+
+
